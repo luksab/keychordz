c8f84ba0943816edcfaa388d1dbcf8f7e133865b
luksab <lukas@sabatschus.de>
2022-04-16 17:49:12 +0200

    use walkdir for orders of magnitude better perf

--- keystats/src/main.rs
+++ keystats/src/main.rs
@@ -1,36 +1,64 @@
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    sync::{atomic::AtomicUsize, Arc, Mutex, RwLock},
+};
 
-use glob::glob;
+use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
+use walkdir::{DirEntry, WalkDir};
 
-fn file_filter(path: &PathBuf) -> bool {
+fn file_filter(path: &DirEntry) -> bool {
+    // println!("{:?}", path);
     let file_exts = vec!["rs", "py", "txt", "md"];
-    let file_name = path.file_name().unwrap().to_str().unwrap();
+    let file_name = path.file_name().to_str().unwrap();
     let file_ext = file_name.split(".").last().unwrap();
     file_exts.contains(&file_ext)
 }
 
 fn main() {
-    let mut ascii_nums = [0; 128];
+    let ascii_nums = Arc::new(Mutex::new([0; 128]));
 
-    for entry in glob("**/*")
-        .unwrap()
+    let mut now = Arc::new(RwLock::new(std::time::SystemTime::now()));
+    let num_files = AtomicUsize::new(0);
+    let glob = WalkDir::new(".")
+        .into_iter()
         .filter_map(Result::ok)
         .filter(file_filter)
-    {
-        // read file to string
-        let file_content = std::fs::read_to_string(&entry).unwrap();
+        .collect::<Vec<_>>();
+
+    println!("{:?}", glob.len());
+
+    glob.par_iter().for_each(|entry| {
+        num_files.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        // read file to u8 otherwise skip
+        let file_content = std::fs::read(entry.path()).unwrap_or(vec![]);
         // get file_content length
-        println!("{}: {}", entry.display(), file_content.len());
+        // println!("{}: {}", entry.display(), file_content.len());
         // count ascii chars
-        for char in file_content.bytes() {
-            ascii_nums[char as usize] += 1;
+        let mut ascii_num = [0; 128];
+        for char in file_content {
+            ascii_num.get_mut(char as usize).map(|c| *c += 1);
+            // ascii_nums[char as usize] += 1;
         }
-    }
+
+        ascii_nums.lock().unwrap().iter_mut().zip(&ascii_num).for_each(|(ascii, asciii)| {
+            *ascii += asciii;
+        });
+
+        if now.read().unwrap().elapsed().unwrap().as_secs() > 1 {
+            println!(
+                "{} files processed",
+                num_files.load(std::sync::atomic::Ordering::Relaxed)
+            );
+            *now.write().unwrap() = std::time::SystemTime::now();
+        }
+    });
 
     // print ascii chars
-    for (i, num) in ascii_nums.iter().enumerate() {
+    for (i, num) in ascii_nums.lock().unwrap().iter().enumerate() {
         if *num > 0 {
             println!("{}: {}", i as u8 as char, num);
         }
     }
+
+    println!("{} files in {:?}", num_files.load(std::sync::atomic::Ordering::Relaxed), now.read().unwrap().elapsed().unwrap());
 }
