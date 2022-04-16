f5c290f763395a0b8f8e1ff0354e9f814f388700
luksab <lukas@sabatschus.de>
2022-04-16 18:17:16 +0200

    add more stats

--- keystats/src/main.rs
+++ keystats/src/main.rs
@@ -1,9 +1,6 @@
-use std::{
-    path::PathBuf,
-    sync::{atomic::AtomicUsize, Arc, Mutex, RwLock},
-};
+use std::sync::{atomic::AtomicUsize, Arc, Mutex, RwLock};
 
-use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use walkdir::{DirEntry, WalkDir};
 
 fn file_filter(path: &DirEntry) -> bool {
@@ -17,7 +14,8 @@ fn file_filter(path: &DirEntry) -> bool {
 fn main() {
     let ascii_nums = Arc::new(Mutex::new([0; 128]));
 
-    let mut now = Arc::new(RwLock::new(std::time::SystemTime::now()));
+    let now = Arc::new(RwLock::new(std::time::SystemTime::now()));
+    let now_total = std::time::SystemTime::now();
     let num_files = AtomicUsize::new(0);
     let glob = WalkDir::new(".")
         .into_iter()
@@ -25,7 +23,8 @@ fn main() {
         .filter(file_filter)
         .collect::<Vec<_>>();
 
-    println!("{:?}", glob.len());
+    let glob_len = glob.len();
+    println!("{} files in total", glob.len());
 
     glob.par_iter().for_each(|entry| {
         num_files.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
@@ -40,25 +39,53 @@ fn main() {
             // ascii_nums[char as usize] += 1;
         }
 
-        ascii_nums.lock().unwrap().iter_mut().zip(&ascii_num).for_each(|(ascii, asciii)| {
-            *ascii += asciii;
-        });
+        // std::thread::sleep(std::time::Duration::from_millis(10));
+
+        ascii_nums
+            .lock()
+            .unwrap()
+            .iter_mut()
+            .zip(&ascii_num)
+            .for_each(|(ascii, asciii)| {
+                *ascii += asciii;
+            });
 
         if now.read().unwrap().elapsed().unwrap().as_secs() > 1 {
+            let num_processed = num_files.load(std::sync::atomic::Ordering::Relaxed);
+            let percent_done = num_processed as f32 / glob_len as f32;
+            let time = now_total.elapsed().unwrap().as_secs_f32();
+            let eta = (time / percent_done) - time;
             println!(
-                "{} files processed",
-                num_files.load(std::sync::atomic::Ordering::Relaxed)
+                "{}/{} files processed. {}% done. ETA: {:?}",
+                num_files.load(std::sync::atomic::Ordering::Relaxed),
+                glob_len,
+                percent_done * 100.,
+                std::time::Duration::from_secs_f32(eta)
             );
             *now.write().unwrap() = std::time::SystemTime::now();
         }
     });
 
     // print ascii chars
+    let mut sum = 0;
+    let mut ascii_lut = vec![];
     for (i, num) in ascii_nums.lock().unwrap().iter().enumerate() {
-        if *num > 0 {
+        if *num > 0 && (32..127).contains(&i) {
             println!("{}: {}", i as u8 as char, num);
+            ascii_lut.push((i as u8 as char, *num));
+            sum += *num;
         }
     }
+    println!("total chars: {}", sum);
 
-    println!("{} files in {:?}", num_files.load(std::sync::atomic::Ordering::Relaxed), now.read().unwrap().elapsed().unwrap());
+    println!("{:?}", ascii_lut);
+    // sort by num
+    ascii_lut.sort_by(|a, b| b.1.cmp(&a.1));
+    println!("{:?}", ascii_lut);
+
+    println!(
+        "{} files in {:?}",
+        num_files.load(std::sync::atomic::Ordering::Relaxed),
+        now.read().unwrap().elapsed().unwrap()
+    );
 }
